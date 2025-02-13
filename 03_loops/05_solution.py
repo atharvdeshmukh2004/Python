123f0ccac9b47ef41e64eb08d9a07caf56f32be7
atharvdeshmukh2004 <datharv56@gmail.com>
2025-02-13 13:02:33 +0530

    Create 05_solution.py

---
+++ 03_loops/05_solution.py
@@ -0,0 +1,11 @@
+"""
+5. Find the First Non-Repeated Character
+Problem: Given a string, find the first non-repeated character.
+
+"""
+input_str = "abcdteterdacb"
+
+for char in input_str:
+    if input_str.count(char)==1:
+        print( "Char is:",char)
+        break
